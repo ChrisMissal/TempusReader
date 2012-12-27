4a8cb0f576c9449c5c7c528c701bbfc696ad576a
Sharon Cichelli <sharon@invisible-city.com>
2012-12-26 22:48:56 -0500

    expressing expected values in tests as relative dates instead of hardcoded

--- src/Tests/DateTests.cs
+++ src/Tests/DateTests.cs
@@ -8,13 +8,7 @@ namespace Tests
     [TestFixture]
     public class DateTests
     {
-        private DateTime BaseDate { get; set; }
-
-        [TestFixtureSetUp]
-        public void TestFixtureSetUp()
-        {
-            BaseDate = new DateTime(1982, 10, 21, 23, 40, 0);
-        }
+        public static readonly DateTime BaseDate = new DateTime(1982, 10, 21, 23, 40, 0);
 
         [TestCaseSource("RelativeTimeTestData")]
         public Date Date_from_string(string input)
@@ -27,13 +21,13 @@ namespace Tests
             get
             {
                 yield return new TestCaseData("in 10 minutes")
-                    .Returns(new Date(new DateTime(1982, 10, 21, 23, 50, 0)));
+                    .Returns(new Date(BaseDate.AddMinutes(10)));
                 yield return new TestCaseData("4 hrs from now")
-                    .Returns(new Date(new DateTime(1982, 10, 22, 3, 40, 0)));
+                    .Returns(new Date(BaseDate.AddHours(4)));
                 yield return new TestCaseData("45 seconds ago")
-                    .Returns(new Date(new DateTime(1982, 10, 21, 23, 39, 15)));
+                    .Returns(new Date(BaseDate.AddSeconds(-45)));
                 yield return new TestCaseData("yesterday")
-                    .Returns(new Date(new DateTime(1982, 10, 20, 23, 40, 0)));
+                    .Returns(new Date(BaseDate.AddDays(-1)));
             }
         }
     }
