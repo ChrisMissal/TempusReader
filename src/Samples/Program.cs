318606f96e43bb1e90f63007eea86acac86994e6
Chris Missal <chris.missal@gmail.com>
2012-12-29 15:55:04 -0600

    added samples console app for testing real use cases

---
+++ src/Samples/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TempusReader;
+
+namespace Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a discount name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Discount expires in:");
+            var expires = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(expires))
+                return;
+
+            TimeSpan expirationDate = new Time(expires);
+            var discount = new Discount(name, DateTime.Now.Add(expirationDate));
+
+            Console.WriteLine(discount);
+
+            Main(args);
+        }
+    }
+
+    public class Discount
+    {
+        private readonly string _name;
+        private readonly DateTime _expirationDate;
+
+        public Discount(string name, DateTime expirationDate)
+        {
+            _name = name;
+            _expirationDate = expirationDate;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Discount: ( Name = '{0}', Expires = '{1:yyyy-MM-dd HH:mm:ss.iii}' )",
+                _name, _expirationDate);
+        }
+    }
+}
